fa2ea3b806cbcc9abac780c5403bb30bd99a5805
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2017-01-27 00:31:33 -0600

    experiment with elm-dom

--- demo/Demo.elm
+++ demo/Demo.elm
@@ -1,32 +1,86 @@
 module Demo exposing (main)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import TreeView exposing (Node(..))
 
 
+stylesheet =
+    let
+        tag =
+            "link"
+
+        attrs =
+            [ attribute "rel" "stylesheet"
+            , attribute "property" "stylesheet"
+            , attribute "href" "demo.css"
+            ]
+
+        children =
+            []
+    in
+        node tag attrs children
+
+
 main =
-    TreeView.treeView config
-        [ Node
-            { id = "1"
-            , text = "1. One"
-            , children =
-                [ Node { id = "1.1", text = "1.1 One-One", children = [] }
-                , Node { id = "1.2", text = "1.2 One-Two", children = [] }
-                ]
-            }
-        , Node
-            { id = "2"
-            , text = "2. Two"
-            , children = []
-            }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+type alias Model =
+    { state : TreeView.InternalState }
+
+
+init =
+    ( { state = TreeView.initialState }, Cmd.none )
+
+
+view model =
+    div []
+        [ stylesheet
+        , TreeView.treeView config
+            model.state
+            [ Node
+                { id = "1"
+                , text = "1. One"
+                , children =
+                    [ Node { id = "1.1", text = "1.1 One-One", children = [] }
+                    , Node { id = "1.2", text = "1.2 One-Two", children = [] }
+                    ]
+                }
+            , Node
+                { id = "2"
+                , text = "2. Two"
+                , children = []
+                }
+            ]
         ]
 
 
 config : TreeView.Config Msg
 config =
-    { toMsg = TreeViewStateChange, onChecked = TreeViewChecked, useCheckbox = False }
+    { onState = StateChange, onChecked = Checked, useCheckbox = True }
 
 
 type Msg
-    = TreeViewStateChange TreeView.InternalState
-    | TreeViewChecked (List String)
+    = StateChange TreeView.InternalState
+    | Checked (List String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        StateChange internalState ->
+            { model | state = internalState } ! []
+
+        Checked list ->
+            model ! []
--- src/TreeView.elm
+++ src/TreeView.elm
@@ -1,8 +1,35 @@
-module TreeView exposing (treeView, InternalState, Config, Data, Node(..), NodeValue)
+module TreeView
+    exposing
+        ( treeView
+        , InternalState
+        , Config
+        , Data
+        , Node(..)
+        , NodeValue
+        , initialState
+        , subscriptions
+        )
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Dict exposing (Dict)
+import Json.Decode
+import AnimationFrame
+import Time
+import DOM
+
+
+subscriptions : Config msg -> InternalState -> Sub msg
+subscriptions config internalState =
+    let
+        state =
+            unboxState internalState
+
+        updateState time =
+            config.onState <| InternalState { state | currentTime = time }
+    in
+        AnimationFrame.times updateState
 
 
 type alias Data =
@@ -18,35 +45,129 @@ type alias NodeValue =
 
 
 type alias State =
-    {}
+    { expandedNodes : Dict String ()
+    , currentTime : Time.Time
+    , heights : Dict String Float
+    }
+
+
+initialState : InternalState
+initialState =
+    InternalState
+        { expandedNodes = Dict.empty
+        , currentTime = 0
+        , heights = Dict.empty
+        }
 
 
 type InternalState
     = InternalState State
 
 
+unboxState : InternalState -> State
+unboxState internalState =
+    case internalState of
+        InternalState state ->
+            state
+
+
 type alias Config msg =
-    { toMsg : InternalState -> msg
+    { onState : InternalState -> msg
     , onChecked : List String -> msg
     , useCheckbox : Bool
     }
 
 
-treeView : Config msg -> Data -> Html msg
-treeView config data =
-    ul [ style [ ( "padding-left", "0" ) ] ]
-        (List.map (unbox >> renderNodeValue config) data)
+defaultConfig : (InternalState -> msg) -> (List String -> msg) -> Config msg
+defaultConfig onState onChecked =
+    { onState = onState
+    , onChecked = onChecked
+    , useCheckbox = True
+    }
 
 
-renderNodeValue : Config msg -> NodeValue -> Html msg
-renderNodeValue config nodeValue =
-    li [ style [ ( "list-style-type", "none" ), ( "", "" ) ] ]
-        [ if config.useCheckbox then
-            label [] [ input [ type_ "checkbox" ] [], text nodeValue.text ]
-          else
-            text nodeValue.text
-        , ul [] (List.map (unbox >> renderNodeValue config) nodeValue.children)
-        ]
+treeView : Config msg -> InternalState -> Data -> Html msg
+treeView config internalState data =
+    ul [ class "treeview" ]
+        (List.map (unbox >> renderNodeValue config (unboxState internalState)) data)
+
+
+renderNodeValue : Config msg -> State -> NodeValue -> Html msg
+renderNodeValue config state nodeValue =
+    let
+        collapsed =
+            not <| Dict.member nodeValue.id state.expandedNodes
+
+        hasChildren =
+            not <| List.isEmpty nodeValue.children
+    in
+        li []
+            [ span [ classList [ ( "arrow", True ), ( "no-children", not hasChildren ) ], arrowClick config state nodeValue.id ]
+                [ case ( hasChildren, collapsed ) of
+                    ( True, True ) ->
+                        text "▶"
+
+                    ( True, False ) ->
+                        text "▼"
+
+                    _ ->
+                        text ""
+                ]
+            , if config.useCheckbox then
+                label [] [ input [ type_ "checkbox" ] [], text nodeValue.text ]
+              else
+                text nodeValue.text
+            , ul
+                [ class <|
+                    "node "
+                        ++ (if collapsed then
+                                "collapsed"
+                            else
+                                "expanded"
+                           )
+                , style <|
+                    if collapsed then
+                        [ ( "height", "0px" ) ]
+                    else
+                        [ ( "height", (Dict.get nodeValue.id state.heights |> Maybe.withDefault 0 |> toString) ++ "px" ) ]
+                ]
+                (List.map (unbox >> renderNodeValue config state) nodeValue.children)
+            ]
+
+
+arrowClick : Config msg -> State -> String -> Html.Attribute msg
+arrowClick config state id =
+    let
+        collapsed =
+            not <| Dict.member id state.expandedNodes
+
+        updateExpandedNodes =
+            if collapsed then
+                Dict.insert id ()
+            else
+                Dict.remove id
+
+        updatedState height =
+            state.expandedNodes
+                |> updateExpandedNodes
+                |> (\expandedNodes ->
+                        { state
+                            | expandedNodes = expandedNodes
+                            , heights =
+                                if height > 0 then
+                                    Dict.insert id height state.heights
+                                else
+                                    state.heights
+                        }
+                   )
+                |> InternalState
+                |> config.onState
+
+        decode : Json.Decode.Decoder Float
+        decode =
+            DOM.target <| DOM.nextSibling <| DOM.nextSibling (DOM.boundingClientRect |> Json.Decode.map .height)
+    in
+        on "click" (decode |> Json.Decode.map (updatedState))
 
 
 unbox : Node -> NodeValue
